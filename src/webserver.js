d3e09d450dedbeaf5a06aefd51caaa92d6240e00
max <max@suedien.de>
2020-04-23 19:01:34 +0200

    fertig für heute yeah

---
+++ src/webserver.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
